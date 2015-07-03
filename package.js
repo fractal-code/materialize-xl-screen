b03d022555e0eb2d6ceafcceea79f2e9d941a4fe
Rami Jarrar <ramijarrar@outlook.com>
2015-07-03 18:39:24 +1000

    Add basic package description

---
+++ package.js
@@ -0,0 +1,6 @@
+Package.describe({
+    name: 'gliese-technology:materialize-xl-screen',
+    version: '0.0.1',
+    summary: 'Adds an extra large screen size to Materialize',
+    git: 'https://github.com/gliese-technology/materialize-xl.git'
+});
