a9fd1bb583b97ce3d5a17c8721daeaa44472e983
Rami Jarrar <ramijarrar@outlook.com>
2015-07-03 19:29:00 +1000

    Change xl screen size to 1366px

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'gliese:materialize-xl-screen',
     version: '0.1.1',
-    summary: 'Adds an extra large screen size (>1200px) to Materialize',
+    summary: 'Adds an extra large screen size (>1366px) to Materialize',
     git: 'https://github.com/gliese-technology/materialize-xl-screen.git'
 });
 
