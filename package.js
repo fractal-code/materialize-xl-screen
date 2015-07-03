434197f2ce25fda01238458fc07a4ed14f30f2f1
Rami Jarrar <ramijarrar@outlook.com>
2015-07-03 19:02:56 +1000

    Bump version to 0.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'gliese:materialize-xl-screen',
-    version: '0.0.1',
+    version: '0.1.0',
     summary: 'Adds an extra large screen size to Materialize',
     git: 'https://github.com/gliese-technology/materialize-xl-screen.git'
 });
