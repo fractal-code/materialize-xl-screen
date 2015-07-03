aff7e070712763f729c19920f162617e4b1bdbaf
Rami Jarrar <ramijarrar@outlook.com>
2015-07-03 18:54:49 +1000

    Update git link to new repo

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'gliese:materialize-xl-screen',
     version: '0.0.1',
     summary: 'Adds an extra large screen size to Materialize',
-    git: 'https://github.com/gliese-technology/materialize-xl.git'
+    git: 'https://github.com/gliese-technology/materialize-xl-screen.git'
 });
 
 Package.onUse(function (api) {
